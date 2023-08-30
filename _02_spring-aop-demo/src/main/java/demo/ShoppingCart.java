34dfcb2b61008f3a0c0431732166c0842f9abeeb
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-08-30 13:40:33 +0200

    created project for aop

---
+++ _02_spring-aop-demo/src/main/java/demo/ShoppingCart.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ShoppingCart {
+
+    public void checkout() {
+        System.out.println("Checkout Method from ShoppingCart called");
+    }
+
+}
