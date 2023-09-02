9a207a84210aeacd3faaebd11481412782c7f0db
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-09-02 22:28:23 +0200

    performed basic crud operations

---
+++ _03_springboot/src/main/java/com/ibo/_03_springboot/repository/DepartmentRepository.java
@@ -0,0 +1,10 @@
+package com.ibo._03_springboot.repository;
+
+import com.ibo._03_springboot.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+}
---
+++ _03_springboot/src/main/java/com/ibo/_03_springboot/service/DepartmentService.java
@@ -0,0 +1,17 @@
+package com.ibo._03_springboot.service;
+
+import com.ibo._03_springboot.entity.Department;
+
+import java.util.List;
+
+public interface DepartmentService {
+    public Department saveDepartment(Department department);
+
+    public List<Department> fetchDepartmentList();
+
+    public Department fetchDepartmentById(Long departmentId);
+
+    public void deleteDepartmentById(Long departmentId);
+
+    public Department updateDepartment(Long departmentId, Department department);
+}
