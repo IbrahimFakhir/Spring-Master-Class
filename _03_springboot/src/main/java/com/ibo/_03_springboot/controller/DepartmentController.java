23bb9eb71a44cd7b294cc508157b2c6dee3307f0
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-09-11 19:17:53 +0200

    added logger

--- _03_springboot/src/main/java/com/ibo/_03_springboot/controller/DepartmentController.java
+++ _03_springboot/src/main/java/com/ibo/_03_springboot/controller/DepartmentController.java
@@ -2,6 +2,8 @@ package com.ibo._03_springboot.controller;
 
 import com.ibo._03_springboot.entity.Department;
 import com.ibo._03_springboot.service.DepartmentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,13 +16,17 @@ public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
 
+    private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+
     @PostMapping("/departments")
     public Department saveDepartment(@Valid @RequestBody Department department) {
+        LOGGER.info("Inside saveDepartment of DepartmentController");
         return departmentService.saveDepartment(department);
     }
 
     @GetMapping("/departments")
     public List<Department> fetchDepartmentList() {
+        LOGGER.info("Inside fetchDepartmentList of DepartmentController");
         return departmentService.fetchDepartmentList();
     }
 
