47c68d7613ad4948243caebabae8e7ce0e791653
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-10-08 17:11:50 +0200

    added project for data jpa tutorial

---
+++ _04_spring-data-jpa/src/main/java/com/ibo/_04_springdatajpa/Application.java
@@ -0,0 +1,13 @@
+package com.ibo._04_springdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
