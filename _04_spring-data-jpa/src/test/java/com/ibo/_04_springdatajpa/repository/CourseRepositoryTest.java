b2b05c65ca213d1e1e997ba35aad324627583cc9
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-10-13 01:52:57 +0200

    added pagination and sorting methods

--- _04_spring-data-jpa/src/test/java/com/ibo/_04_springdatajpa/repository/CourseRepositoryTest.java
+++ _04_spring-data-jpa/src/test/java/com/ibo/_04_springdatajpa/repository/CourseRepositoryTest.java
@@ -5,6 +5,9 @@ import com.ibo._04_springdatajpa.entity.Teacher;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
 
@@ -37,6 +40,40 @@ class CourseRepositoryTest {
                 .build();
 
         courseRepository.save(course);
+    }
+
+    @Test
+    public void findAllPagination() {
+        Pageable firstPageWithThreeRecords = PageRequest.of(0, 3);
+        Pageable secondPageWithTwoRecords = PageRequest.of(1, 2);
+
+        List<Course> courses = courseRepository.findAll(firstPageWithThreeRecords).getContent();
+
+        long totalElements = courseRepository.findAll(firstPageWithThreeRecords).getTotalElements();
+        long totalPages = courseRepository.findAll(firstPageWithThreeRecords).getTotalPages();
+
+        System.out.println("totalElements = " + totalElements);
+        System.out.println("totalPages = " + totalPages);
+
+        System.out.println("courses = " + courses);
+    }
+
+    @Test
+    public void findAllSorting() {
+        Pageable sortByTitle = PageRequest.of(
+                0,
+                2,
+                Sort.by("title")
+        );
+        Pageable sortByCreditDesc = PageRequest.of(
+                0,
+                2,
+                Sort.by("credit").descending()
+        );
+
+        List<Course> courses = courseRepository.findAll(sortByTitle).getContent();
+
+        System.out.println("courses = " + courses);
     }
 
 }
