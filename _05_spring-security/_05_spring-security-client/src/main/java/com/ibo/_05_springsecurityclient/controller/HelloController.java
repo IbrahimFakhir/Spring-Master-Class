c0d902699df1151c31e53553fd348ca93ce64bb6
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-12-03 00:38:36 +0100

    added hellocontroller

---
+++ _05_spring-security/_05_spring-security-client/src/main/java/com/ibo/_05_springsecurityclient/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.ibo._05_springsecurityclient.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+
+}
